73430b92b29bf0d30e1f313481d214b4e17ffba7
Reda-23 <readhajjaj@gmail.com>
2022-12-04 17:27:24 +0100

    Spring Batch Item Processor  Class

--- src/main/java/sid/springbatch/batch/FootballItemProcessor.java
+++ src/main/java/sid/springbatch/batch/FootballItemProcessor.java
@@ -1,10 +1,9 @@
 package sid.springbatch.batch;
 
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.stereotype.Component;
+
 import sid.springbatch.entity.Football;
 
-@Component
 public class FootballItemProcessor implements ItemProcessor<Football,Football> {
 
     @Override
