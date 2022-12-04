7ff7a7472d6866869be5a7360d3a7ac8f4d03802
Reda-23 <readhajjaj@gmail.com>
2022-12-04 17:27:52 +0100

    Spring Batch Config

--- src/main/java/sid/springbatch/batch/FootballItemWriter.java
+++ src/main/java/sid/springbatch/batch/FootballItemWriter.java
@@ -2,17 +2,19 @@ package sid.springbatch.batch;
 
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import sid.springbatch.entity.Football;
 import sid.springbatch.repository.FootballRepository;
 
 import java.util.List;
 
+@Component
 public class FootballItemWriter implements ItemWriter<Football> {
 
     @Autowired
     private FootballRepository footballRepository;
     @Override
     public void write(List<? extends Football> list) throws Exception {
-            footballRepository.saveAll(list);
+        footballRepository.saveAll(list);
     }
 }
--- src/main/java/sid/springbatch/batch/SpringBatchConfig.java
+++ src/main/java/sid/springbatch/batch/SpringBatchConfig.java
@@ -1,6 +1,5 @@
 package sid.springbatch.batch;
 
-import lombok.AllArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -11,61 +10,84 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+
+
+import org.springframework.core.io.Resource;
 import sid.springbatch.entity.Football;
+import sid.springbatch.repository.FootballRepository;
 
 @Configuration
 @EnableBatchProcessing
-@AllArgsConstructor
 public class SpringBatchConfig {
 
+    @Autowired
     private JobBuilderFactory jobBuilder;
+    @Autowired
     private StepBuilderFactory stepBuilder;
-    private ItemWriter<Football> itemWriter;
-    private ItemProcessor<Football,Football> itemProcessor;
-    private ItemReader<Football> itemReader;
+    @Autowired
+    private ItemWriter<Football> footballItemWriter;
+    @Autowired
+    private ItemProcessor<Football,Football> footballItemProcessor;
+    @Autowired
+    private ItemReader<Football> footballItemReader;
 
 
-    public Job job(){
+
+
+
+    @Bean
+    public Job footJob(){
         Step step1 = stepBuilder.get("ETL-Load-File")
                 .<Football,Football>chunk(100)
-                .reader(itemReader)
-                .processor(itemProcessor)
-                .writer(itemWriter)
+                .reader(footballItemReader)
+                .processor(footballItemProcessor)
+                .writer(footballItemWriter)
                 .build();
 
         return jobBuilder.get("ETL-read").start(step1).build();
     }
 
-    public FlatFileItemReader<Football> reader(){
+    @Bean
+    FootballItemProcessor itemProcessor(){
+        return new FootballItemProcessor();
+    }
+
+    @Bean
+    public FlatFileItemReader<Football> reader(@Value("${inputFile}") Resource inputFile){
         FlatFileItemReader<Football> flatFileItemReader = new FlatFileItemReader<>();
         flatFileItemReader.setName("Football");
-        flatFileItemReader.setResource(new FileSystemResource("src/main/resources/stadium.csv"));
+        flatFileItemReader.setResource(inputFile);
         flatFileItemReader.setLinesToSkip(1);
         flatFileItemReader.setLineMapper(lineMapper());
 
         return flatFileItemReader;
     }
 
-    private LineMapper<Football> lineMapper() {
+    @Bean
+    public LineMapper<Football> lineMapper() {
         DefaultLineMapper<Football> lineMapper = new DefaultLineMapper<>();
         DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
         lineTokenizer.setDelimiter(",");
         lineTokenizer.setStrict(false);
         lineTokenizer.setNames("team","city","stadium","capacity","latitude","longitude","country");
-
+        lineMapper.setLineTokenizer(lineTokenizer);
         BeanWrapperFieldSetMapper<Football> beanWrapperFieldSetMapper = new BeanWrapperFieldSetMapper<>();
         beanWrapperFieldSetMapper.setTargetType(Football.class);
         lineMapper.setFieldSetMapper(beanWrapperFieldSetMapper);
         return lineMapper;
     }
+
 
 
 }
